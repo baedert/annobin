a1c6bdeb0745f224dcffeb054c9f1456bf6728d9
Nick Clifton <nickc@redhat.com>
2020-05-12 14:27:25 +0100

    llvm plugin update

--- llvm-plugin/annobin.cpp
+++ llvm-plugin/annobin.cpp
@@ -1,30 +1,69 @@
 #include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
 using namespace llvm;
-
-namespace {
-  struct SkeletonPass : public FunctionPass {
+namespace
+{
+  class AnnobinModulePass : public ModulePass
+  {
+  public:
     static char ID;
-    SkeletonPass() : FunctionPass(ID) {}
+    AnnobinModulePass() : ModulePass (ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
+    virtual bool
+    runOnModule (Module &M)
+    {
+      if (M.getPICLevel() == PICLevel::NotPIC)
+	errs() << "no PIC\n";
+      else
+	errs() << "PIC enabled\n";
+      
+      return false; // Module not modified.
+    }
+  };
+
+  Pass *
+  createAnnobinModulePass (void)
+  {
+    errs() << "Creating Module Pass\n";
+    return new AnnobinModulePass;
+  }
+  
+  class AnnobinFunctionPass : public FunctionPass
+  {
+    static char ID;
+  public:
+    AnnobinFunctionPass() : FunctionPass (ID) {}
+
+    virtual bool
+    runOnFunction (Function & F)
+    {
+      errs() << "Checking function " << F.getName () << "\n";
       return false;
     }
   };
 }
 
-char SkeletonPass::ID = 0;
+char AnnobinModulePass::ID = 0;
+char AnnobinFunctionPass::ID = 0;
 
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
-static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new SkeletonPass());
+static void
+registerAnnobinPasses (const PassManagerBuilder & PMB,
+		       legacy::PassManagerBase & PM)
+{
+  static RegisterPass<AnnobinModulePass> X("annobin", "Annobin Module Pass");
+
+  PM.add (new AnnobinFunctionPass ());
+
+  // Warning: PM.add() can only be used to add Function passes.
+  // It does not work for Module passes.
+  PM.add (createAnnobinModulePass());
+  
+  errs() << "Optimization level is " << PMB.OptLevel << "\n";
 }
+
 static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
+RegisterMyPass (PassManagerBuilder::EP_ModuleOptimizerEarly, registerAnnobinPasses);
